1dace408924f806ff98169f9086f24eb2c655151
kartikeyapande <intupande@gmail.com>
2017-02-02 19:00:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+console.log('Loaded')
+console.log('Loaded')
+console.log('Loaded')
+console.log('Loaded')
+console.log('Loaded')
+console.log('Loaded')
+console.log('Great it has jst loaded')
